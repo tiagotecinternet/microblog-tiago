2acdb5bd733b7537e90917f45495444b7a3500e7
tiagotecinternet <tiago.tecinternet22@gmail.com>
2022-07-22 10:06:16 -0300

    Modelagem física e classe Banco
    Uso de namespace e autoload com composer

---
+++ src/Banco.php
@@ -0,0 +1,30 @@
+<?php
+namespace Microblog;
+use PDO, Exception;
+
+abstract class Banco {
+    private static string $servidor = "localhost";
+    private static string $usuario = "root";
+    private static string $senha = "";
+    private static string $banco = "microblog_tiago";
+    private static PDO $conexao; 
+
+    public static function conecta():PDO {
+        try {
+            self::$conexao = new PDO(
+                "mysql:host=".self::$servidor."; 
+                dbname=".self::$banco.";
+                charset=utf8",
+                self::$usuario, 
+                self::$senha
+            );
+            self::$conexao->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            //echo "Ok!"; // teste
+        } catch (Exception $erro) {
+            die("Deu ruim: ".$erro->getMessage());
+        }
+        return self::$conexao;
+    }
+}
+//Banco::conecta(); // teste
