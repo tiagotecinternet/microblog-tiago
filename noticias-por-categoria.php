b8daa10e8fe35a33fea5110e1271217ec5652789
tiagotecinternet <tiago.tecinternet22@gmail.com>
2022-08-17 09:22:27 -0300

    Update noticias-por-categoria.php

--- noticias-por-categoria.php
+++ noticias-por-categoria.php
@@ -1,37 +1,40 @@
 <?php
 use Microblog\Utilitarios;
-
 require_once "inc/cabecalho.php";
 $noticia->setCategoriaId($_GET['id']);
 $dados = $noticia->listarPorCategoria();
-Utilitarios::dump($dados);
+// Utilitarios::dump($dados);
 ?>
 
 
 <div class="row my-1 mx-md-n1">
 
     <article class="col-12">
-        <h2 class=" ">Notícias sobre <span class="badge bg-primary">categoria</span> </h2>
-        
+    <?php if( count($dados) > 0 ) { ?>
+        <h2>Notícias sobre 
+        <span class="badge bg-primary"> 
+            <?=$dados[0]['categoria']?> 
+        </span> 
+        </h2>
+    <?php } else { ?>
+        <h2 class="alert alert-warning text-center">
+            Não tem notícias desta categoria!</h2>
+    <?php } ?>
+
         <div class="row my-1">
             <div class="col-12 px-md-1">
                 <div class="list-group">
-                    <a href="noticia.php" class="list-group-item list-group-item-action">
-                        <h3 class="fs-6">Título da notícia</h3>
-                        <p><time>12/12/2012</time> - Autor da notícia</p>
-                        <p>Resumo da notícia</p>
-                    </a>
-                    <a href="noticia.php" class="list-group-item list-group-item-action">
-                        <h3 class="fs-6">Título da notícia</h3>
-                        <p><time>12/12/2012</time> - Autor da notícia</p>
-                        <p>Resumo da notícia</p>
-                    </a>
-                    <a href="noticia.php" class="list-group-item list-group-item-action">
-                        <h3 class="fs-6">Título da notícia</h3>
-                        <p><time>12/12/2012</time> - Autor da notícia</p>
-                        <p>Resumo da notícia</p>
-                    </a>
-                    
+
+                <?php foreach($dados as $dado) { ?>
+                    <a href="noticia.php?id=<?=$dado['id']?>" class="list-group-item list-group-item-action">
+                        <h3 class="fs-6"><?=$dado['titulo']?></h3>
+                        <p><time>
+                        <?=Utilitarios::formataData($dado['data'])?>
+                        </time> - <?=$dado['autor'] ?? "<i>Equipe Microblog</i>"?></p>
+                        <p><?=$dado['resumo']?></p>
+                    </a>                                       
+                <?php } ?>
+
                 </div>
             </div>
         </div>
